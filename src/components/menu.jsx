9e3123ff56e92df44d48b0f82e013bb2079a3f56
Your Nam <you@example.com>
2019-03-01 18:05:16 -0500

    first commit

---
+++ src/components/menu.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import IconButton from "@material-ui/core/IconButton";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+const options = [
+  "None",
+  "Atria",
+  "Callisto",
+  "Dione",
+  "Ganymede",
+  "Hangouts Call",
+  "Luna",
+  "Oberon",
+  "Phobos",
+  "Pyxis",
+  "Sedna",
+  "Titania",
+  "Triton",
+  "Umbriel"
+];
+
+const ITEM_HEIGHT = 88;
+
+class LongMenu extends React.Component {
+  state = {
+    anchorEl: null,
+    selectedItem: ""
+  };
+
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    alert("this");
+    this.setState({ anchorEl: null });
+  };
+
+  render() {
+    const { anchorEl } = this.state;
+    const open = Boolean(anchorEl);
+
+    return (
+      <div>
+        <IconButton
+          aria-label="Menu"
+          aria-owns={open ? "long-menu" : undefined}
+          aria-haspopup="true"
+          onClick={this.handleClick}
+        >
+          <MoreVertIcon />
+        </IconButton>
+        <Menu
+          id="long-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={this.handleClose}
+          PaperProps={{
+            style: {
+              maxHeight: ITEM_HEIGHT * 4.5,
+              width: 500
+            }
+          }}
+        >
+          {options.map(option => (
+            <MenuItem
+              key={option}
+              selected={option === "Pyxis"}
+              onClick={this.handleClose(option)}
+            >
+              {option}
+            </MenuItem>
+          ))}
+        </Menu>
+      </div>
+    );
+  }
+}
+
+export default LongMenu;
