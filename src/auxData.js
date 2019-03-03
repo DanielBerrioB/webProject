0bdf93e2adf399c71f74badcc5f6142af5b3f906
DanielBerrioB <34080611+DanielBerrioB@users.noreply.github.com>
2019-03-02 22:32:37 -0500

    Little change

--- src/auxData.js
+++ src/auxData.js
@@ -1,3 +1,55 @@
+<<<<<<< HEAD
+export default [
+  {
+    id: 1,
+    name: "Buzo con pantalón",
+    source:
+      "http://drive.google.com/uc?export=view&id=1wYGK1zy-VTptuiIDFvh0dcoNFU-_oDXT",
+    precio: 50000,
+    categoria: "pantalon"
+  },
+  {
+    id: 2,
+    name: "Camisas",
+    source:
+      "http://drive.google.com/uc?export=view&id=1-k63xPM6GExVj5zUzMm7gq-SyrmjNjxL",
+    precio: 66000,
+    categoria: "Camisa"
+  },
+  {
+    id: 3,
+    name: "Algo",
+    source:
+      "http://drive.google.com/uc?export=view&id=1wuWVK_pOOEO1all96rKQvimctl_8TGtz",
+    precio: 68000,
+    categoria: "pantalon"
+  },
+  {
+    id: 4,
+    name: "Camisón",
+    source:
+      "http://drive.google.com/uc?export=view&id=1zwd2slAfp7LZSkfS_Xk8iZP7OEFqMsHO",
+    precio: 67000,
+    categoria: "Camisas"
+  },
+  {
+    id: 5,
+    name: "Camisón",
+    source:
+      "http://drive.google.com/uc?export=view&id=1kw0nv3zpqnQZbRerkBjsHhIxmP6wpAh9",
+    precio: 69000,
+    categoria: "Camisas"
+  },
+  {
+    id: 6,
+    name: "Camisón",
+    source:
+      "http://drive.google.com/uc?export=view&id=195NTDKqkBO0yUgmMSnQYPQezmf85vtkg",
+    precio: 80000,
+    categoria: "Camisas"
+  }
+];
+=======
 export default [
   {
     id: 1,
@@ -24,3 +76,4 @@ export default [
     categoria: "pantalon"
   }
 ];
+>>>>>>> 564a14dc02bde10a352a53e31c31bdcd8c290fd3
