74aaa53cbd4ff8e7fc08d5a65ab7b1cb9da203b4
DanielBerrioB <34080611+DanielBerrioB@users.noreply.github.com>
2019-03-02 22:32:02 -0500

    Changes

--- src/components/imageSet.jsx
+++ src/components/imageSet.jsx
@@ -1,3 +1,75 @@
+<<<<<<< HEAD
+import React from "react";
+import Data from "../auxData.js";
+import TableElement from "./tableElement";
+
+const simpleStyle = {
+  width: "100%",
+  marginTop: "50px"
+};
+
+const imagenStyle = {
+  borderRadius: "50px"
+};
+var cont = 0;
+var values = [],
+  aux = [];
+for (let i = 0; i < Data.length; i++) {
+  if (cont === 3) {
+    aux.push(Data[i]);
+    values.push(aux);
+    aux = [];
+    cont = 0;
+  } else {
+    aux.push(Data[i]);
+    cont++;
+  }
+}
+values.push(aux);
+
+//This class represents
+class ImageSet extends React.Component {
+  state = {
+    arrayElement: values
+  };
+
+  render() {
+    return (
+      <div>
+        <center>
+          <div>
+            <table style={simpleStyle} cellSpacing="20px">
+              <tr>
+                {this.state.arrayElement[0].map(option => (
+                  <TableElement
+                    name={option.name}
+                    precio={option.precio}
+                    src={option.source}
+                    style={imagenStyle}
+                  />
+                ))}
+              </tr>
+              <br />
+              <tr>
+                {this.state.arrayElement[1].map(option => (
+                  <TableElement
+                    name={option.name}
+                    precio={option.precio}
+                    src={option.source}
+                    style={imagenStyle}
+                  />
+                ))}
+              </tr>
+            </table>
+          </div>
+        </center>
+      </div>
+    );
+  }
+}
+
+export default ImageSet;
+=======
 import React from "react";
 
 const simpleStyle = {
@@ -183,3 +255,4 @@ class ImageSet extends React.Component {
 }
 
 export default ImageSet;
+>>>>>>> 564a14dc02bde10a352a53e31c31bdcd8c290fd3
