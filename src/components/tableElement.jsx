f95576620c4f9b4acf1969e6d41f3361d83db715
Your Nam <you@example.com>
2019-03-02 22:55:04 -0500

    New change

--- src/components/tableElement.jsx
+++ src/components/tableElement.jsx
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 import React from "react";
 
+//Somethind different
 class TableElement extends React.Component {
   render() {
     return (
@@ -20,26 +20,3 @@ class TableElement extends React.Component {
 }
 
 export default TableElement;
-=======
-import React from "react";
-
-class TableElement extends React.Component {
-  render() {
-    return (
-      <th>
-        <img
-          style={this.props.style}
-          src={this.props.src}
-          width="250"
-          height="430"
-          alt=""
-        />
-        <p>{this.props.name}</p>
-        <p>Precio: {this.props.precio}</p>
-      </th>
-    );
-  }
-}
-
-export default TableElement;
->>>>>>> 0bdf93e2adf399c71f74badcc5f6142af5b3f906
