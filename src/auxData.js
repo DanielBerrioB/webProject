0bcbeed88e7116b38e929937d324f575ab413f7c
celciusjj <37490999+celciusjj@users.noreply.github.com>
2019-03-05 11:14:24 -0500

    Add files via upload

--- src/auxData.js
+++ src/auxData.js
@@ -6,7 +6,8 @@ export default [
       "http://drive.google.com/uc?export=view&id=1wYGK1zy-VTptuiIDFvh0dcoNFU-_oDXT",
     precio: 50000,
     categoria: "pantalon",
-    promocion: true
+    promocion: true,
+    talla: ["S","M","L"]
   },
   {
     id: 2,
@@ -15,7 +16,8 @@ export default [
       "http://drive.google.com/uc?export=view&id=1-k63xPM6GExVj5zUzMm7gq-SyrmjNjxL",
     precio: 66000,
     categoria: "Camisa",
-    promocion: true
+    promocion: true,
+    talla: ["S","M","L"]
   },
   {
     id: 3,
@@ -24,7 +26,8 @@ export default [
       "http://drive.google.com/uc?export=view&id=1wuWVK_pOOEO1all96rKQvimctl_8TGtz",
     precio: 68000,
     categoria: "pantalon",
-    promocion: true
+    promocion: true,
+    talla: ["S","M","L","XL","XXL"]
   },
   {
     id: 4,
@@ -33,7 +36,8 @@ export default [
       "http://drive.google.com/uc?export=view&id=1zwd2slAfp7LZSkfS_Xk8iZP7OEFqMsHO",
     precio: 67000,
     categoria: "Camisa",
-    promocion: false
+    promocion: false,
+    talla: ["S","M","L"]
   },
   {
     id: 5,
@@ -42,7 +46,8 @@ export default [
       "http://drive.google.com/uc?export=view&id=1kw0nv3zpqnQZbRerkBjsHhIxmP6wpAh9",
     precio: 69000,
     categoria: "Camisa",
-    promocion: false
+    promocion: false,
+    talla: ["S","M","L","XL"]
   },
   {
     id: 6,
@@ -51,7 +56,8 @@ export default [
       "http://drive.google.com/uc?export=view&id=195NTDKqkBO0yUgmMSnQYPQezmf85vtkg",
     precio: 80000,
     categoria: "Camisa",
-    promocion: false
+    promocion: false,
+    talla: ["S","M"]
   },
   {
     id: 7,
@@ -60,6 +66,7 @@ export default [
       "http://drive.google.com/uc?export=view&id=195NTDKqkBO0yUgmMSnQYPQezmf85vtkg",
     precio: 80000,
     categoria: "Camisa",
-    promocion: false
+    promocion: false,
+    talla: ["S","M","L","XL","XXL"]
   }
 ];
