1925d13c2f150900a57823ff52ac3d424987f674
JessiParra14 <42593273+JessiParra14@users.noreply.github.com>
2019-03-05 15:24:40 -0600

    cambios prenda

--- src/auxData.js
+++ src/auxData.js
@@ -1,72 +1,62 @@
 export default [
   {
     id: 1,
-    name: "Buzo con pantalón",
+    name: "Gabán",
     source:
       "http://drive.google.com/uc?export=view&id=1wYGK1zy-VTptuiIDFvh0dcoNFU-_oDXT",
     precio: 50000,
-    categoria: "pantalon",
+    categoria: "buzos",
     promocion: true,
     talla: ["S","M","L"]
   },
   {
     id: 2,
-    name: "Camisas",
+    name: "Jean",
     source:
       "http://drive.google.com/uc?export=view&id=1-k63xPM6GExVj5zUzMm7gq-SyrmjNjxL",
-    precio: 66000,
-    categoria: "Camisa",
+    precio: 63000,
+    categoria: "pantalon",
     promocion: true,
     talla: ["S","M","L"]
   },
   {
     id: 3,
-    name: "Algo",
+    name: "Boyfriend",
     source:
       "http://drive.google.com/uc?export=view&id=1wuWVK_pOOEO1all96rKQvimctl_8TGtz",
-    precio: 68000,
+    precio: 66000,
     categoria: "pantalon",
     promocion: true,
     talla: ["S","M","L","XL","XXL"]
   },
   {
     id: 4,
-    name: "Camisón",
+    name: "Vestido",
     source:
       "http://drive.google.com/uc?export=view&id=1zwd2slAfp7LZSkfS_Xk8iZP7OEFqMsHO",
-    precio: 67000,
+    precio: 55000,
     categoria: "Camisa",
     promocion: false,
     talla: ["S","M","L"]
   },
   {
     id: 5,
-    name: "Camisón",
+    name: "Vestido",
     source:
       "http://drive.google.com/uc?export=view&id=1kw0nv3zpqnQZbRerkBjsHhIxmP6wpAh9",
-    precio: 69000,
+    precio: 59000,
     categoria: "Camisa",
     promocion: false,
     talla: ["S","M","L","XL"]
   },
   {
     id: 6,
-    name: "Camisón",
+    name: "Camiseta",
     source:
       "http://drive.google.com/uc?export=view&id=195NTDKqkBO0yUgmMSnQYPQezmf85vtkg",
-    precio: 80000,
+    precio: 45000,
     categoria: "Camisa",
     promocion: false,
     talla: ["S","M"]
-  },
-  {
-    id: 7,
-    name: "Camisón",
-    source:
-      "http://drive.google.com/uc?export=view&id=195NTDKqkBO0yUgmMSnQYPQezmf85vtkg",
-    precio: 80000,
-    categoria: "Camisa",
-    promocion: false,
-    talla: ["S","M","L","XL","XXL"]
   }
 ];
