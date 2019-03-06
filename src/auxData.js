b376d8571e65b052624e3af1ade0a188a4950c65
JessiParra14 <42593273+JessiParra14@users.noreply.github.com>
2019-03-05 18:36:51 -0600

    Update auxData.js

--- src/auxData.js
+++ src/auxData.js
@@ -58,5 +58,203 @@ export default [
     categoria: "Camisa",
     promocion: false,
     talla: ["S","M"]
+  },
+  {
+    id: 7,
+    name: "Blusa",
+    source:"http://drive.google.com/uc?export=view&id=1bSfbcfscRbP4O8_ttrvagn9FwHaWeLRq",
+    precio: 55000,
+    categoria: "Camisa",
+    promocion: false,
+    talla: ["S","M"]
+  },
+  {
+    id: 8,
+    name: "Blusa",
+    source:"http://drive.google.com/uc?export=view&id=1EXMnqNvLiJyR9IEkgV3vG0YHzeghWcor",
+    precio: 58000,
+    categoria: "Camisa",
+    promocion: false,
+    talla: ["S","M", "L"]
+  },
+  {
+    id: 9,
+    name: "Camiseta",
+    source:"http://drive.google.com/uc?export=view&id=1FrAdh3GuRGz8rlk-T_QulA3PEf4UeyXn",
+    precio: 48000,
+    categoria: "Camisa",
+    promocion: false,
+    talla: ["M","L", "XL"]
+  },
+  {
+    id: 10,
+    name: "Vestido",
+    source:"http://drive.google.com/uc?export=view&id=1f-k1TvwJagpE8juHENPWJ35ATibQEkuB",
+    precio: 50000,
+    categoria: "Camisa",
+    promocion: false,
+    talla: ["S","M", "L", "XL"]
+  },
+  {
+    id: 11,
+    name: "Camiseta",
+    source:"http://drive.google.com/uc?export=view&id=1O4ukhLcWW2B87T1XLUZeahpeYgWFfv7d",
+    precio: 35000,
+    categoria: "Camisa",
+    promocion: true,
+    talla: ["S","M"]
+  },
+  {
+    id: 12,
+    name: "Buzo",
+    source:"http://drive.google.com/uc?export=view&id=1aUnsh2Zt7MXMN2jxkcJX8TdXZuFRBh1e",
+    precio: 43000,
+    categoria: "Camisa",
+    promocion: false,
+    talla: ["S","M", "L"]
+  },
+  {
+    id: 13,
+    name: "Buzo",
+    source:"http://drive.google.com/uc?export=view&id=1S0Pdkid-O3DmXFQ2M9ByvqllmIKiDn2A",
+    precio: 45000,
+    categoria: "Camisa",
+    promocion: false,
+    talla: ["S","M"]
+  },
+  {
+    id: 14,
+    name: "Prenda completa",
+    source: "http://drive.google.com/uc?export=view&id=1kIMDWZeDjZUy1Tf3NCVSfjwO_4BMM21F",
+    precio: 65000,
+    categoria: "Camisa",
+    promocion: true,
+    talla: ["S", "M", "XL"]
+  },
+  {
+    id: 15,
+    name: "Prenda completa",
+    source:"http://drive.google.com/uc?export=view&id=1x-R5hOVGd9PN1hj8dd7bLH2gYyJWB2XS",
+    precio: 75000,
+    categoria:"Camisa",
+    promocion: true,
+    talla: ["S", "M", "L"]
+  },
+  {
+    id: 16,
+    name: "Enterizo",
+    source:"http://drive.google.com/uc?export=view&id=16GhL_1ZoqUDe__nsVrEyau6q7E551R8b",
+    precio: 63000,
+    categoria:"Camisa",
+    promocion: false,
+    talla: ["XS", "S", "L"]
+  },
+  {
+    id: 17,
+    name: "Prenda completa",
+    source:"http://drive.google.com/uc?export=view&id=1wlCfHMcZPaBtootJpza1XsfTQeRZBXfG",
+    precio: 85000,
+    categoria:"Camisa",
+    promocion: true,
+    talla: ["S", "M", "L"]
+  },
+  {
+    id: 18,
+    name: "Gabán",
+    source:"http://drive.google.com/uc?export=view&id=1wHWkTANGoXYNjX9OIUSwejllnLIF3pHa",
+    precio: 50000,
+    categoria:"Camisa",
+    promocion: false,
+    talla: ["M", "L", "XL"]
+  },
+  {
+    id: 19,
+    name: "Bleizer",
+    source:"http://drive.google.com/uc?export=view&id=1I-XAlMPyDg62JsnGV9LWMabFHMQ0XvFq",
+    precio: 45000,
+    categoria: "Camisa",
+    promocion: false,
+    talla: ["XS", "S", "M", "L"]
+  },
+  {
+    id: 20,
+    name: "Jean",
+    source:"http://drive.google.com/uc?export=view&id=1BVCFJGXGst2gAOzRbYg4Wq6Wil4-4EsJ",
+    precio: 66000,
+    categoria: "pantalon",
+    promocion: false,
+    talla: ["XS", "S", "M"]
+  },
+  {
+    id: 21,
+    name: "Bodys",
+    source:"http://drive.google.com/uc?export=view&id=1rNqNFT5DrtN21e-0rmDGh97-GH_Tzoye",
+    precio: 38000,
+    categoria: "Camisa",
+    promocion: false,
+    talla: ["XS", "S", "M"]
+  },
+  {
+    id: 22,
+    name: "Gabán",
+    source:"http://drive.google.com/uc?export=view&id=1hy1QASLAISFRc9hdQCBpE2UvPNv4BV_V",
+    precio: 55000,
+    categoria: "Camisa",
+    promocion: true,
+    talla: ["S", "M", "L"]
+  },
+  {
+    id: 23,
+    name: "Blusa",
+    source:"http://drive.google.com/uc?export=view&id=1IQBjA4GKesw7lmiO7kqhutsEEdQRRFZM",
+    precio: 38000,
+    categoria: "Camisa",
+    promocion: false,
+    talla: ["XS", "S", "M", "L"]
+  },
+  {
+    id: 24,
+    name: "Blusa",
+    source:"http://drive.google.com/uc?export=view&id=1uC7LLNflaftYGdPOn-vC5uhNQHDkQcZX",
+    precio: 35000,
+    categoria: "Camisa",
+    promocion: true,
+    talla: ["XS", "S", "M", "L"]
+  },
+  {
+    id: 25,
+    name: "Blusa",
+    source:"http://drive.google.com/uc?export=view&id=1bU9CGStgCfJf_2TEMpb1Pla2Wxpm47ov",
+    precio: 30000,
+    categoria: "Camisa",
+    promocion: true,
+    talla: ["XS", "S", "M"]
+  },
+  {
+    id: 26,
+    name: "Camiseta",
+    source:"http://drive.google.com/uc?export=view&id=1RamI4pty6TpUZhQ3-Sdh5YPKU6Ic4Ps5",
+    precio: 30000,
+    categoria: "Camisa",
+    promocion: false,
+    talla: ["S", "M", "L"]
+  },
+  {
+    id: 27,
+    name: "Gabán",
+    source:"http://drive.google.com/uc?export=view&id=1MF4BA8vtOi1Bat-BTe6SX1UlsLx_G_-X",
+    precio: 45000,
+    categoria: "Camisa",
+    promocion: false,
+    talla: ["S", "M", "L"]
+  },
+  {
+    id: 28,
+    name: "Pinta completa",
+    source:"http://drive.google.com/uc?export=view&id=1LyTVqJWYX6l7_gCJ0XAKo2_uKWJ0DNiR",
+    precio: 80000,
+    categoria: "Camisa",
+    promocion: true,
+    talla: ["XS", "S", "M", "L"]
   }
 ];
